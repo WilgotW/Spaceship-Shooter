e3ae526d3467f991bbf3f7d20a67837baab9cbc5
Wilgot <wilgotwardell@hotmail.com>
2022-06-28 23:25:57 +0200

    Added wave spawner

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
 const canvas = document.querySelector("canvas");
 const c = canvas.getContext('2d');
 
-canvas.width = 1024 * 1.25;
-canvas.height = 560 * 1.25;
+canvas.width = 1024 * 1.4;
+canvas.height = 560 * 1.4;
 
 const astroidAmount = 20;
 const playersAmount = 1;
 const playerMoveForce = 0.05;
 const playerMaxSpeed = 3;
 let bulletCoolDown = 0.2;
+let spawnRepeatRate = 3000; //1 second
+let spawnAmount = 1;
 
+let astroidsSpawned = 0;
 
 let astroids = [];
 let bullets = [];
@@ -21,22 +24,40 @@ let dashInterval;
 let timePassed = 0;
 let coolDownEnded = true;
 
+
 class Astroid {
-    constructor(x, y, width, height, color){
+    constructor(x, y, width, height, color, downSpeed){
         this.x = x;
         this.y = y;
         this.width = width;
         this.height = height;
         this.defaultColor = color;
+        this.fallSpeed = downSpeed;
+        this.destroyed = false;
     }
     draw(){
-        c.fillStyle = this.defaultColor;
-        c.fillRect(this.x, this.y, this.width, this.height);
+        if(!this.destroyed){
+            c.fillStyle = this.defaultColor;
+            c.fillRect(this.x, this.y, this.width, this.height);
+        }
+        
     }
     move(){
         //Random Movement
-        this.x = this.x += Math.ceil(Math.random() * 1) * (Math.round(Math.random()) ? 1 : -1);
-        this.y = this.y += Math.ceil(Math.random() * 1) * (Math.round(Math.random()) ? 1 : -1);
+        // if(!this.destroyed){
+        //     this.x = this.x += Math.ceil(Math.random() * 1) * (Math.round(Math.random()) ? 1 : -1);
+        //     this.y = this.y += Math.ceil(Math.random() * 1) * (Math.round(Math.random()) ? 1 : -1);
+        // }
+        if(!this.destroyed){
+            this.y += this.fallSpeed;
+            this.x += Math.ceil(Math.random() * 1) * (Math.round(Math.random()) ? 1 : -1);
+        }
+    }
+    destroy(){
+        this.destroyed = true;
+        this.x = 1000000;
+        this.width = 0;
+        this.height = 0;
     }
 }
 
@@ -83,19 +104,31 @@ class Bullet {
         this.height = height;
         this.defaultColor = color;
         this.bulletSpeed = 10;
+        this.destroyed = false;
     }
     draw(){
-        c.fillStyle = this.defaultColor;
-        c.fillRect(this.x, this.y, this.width, this.height);
+        if(!this.destroyed){
+            c.fillStyle = this.defaultColor;
+            c.fillRect(this.x, this.y, this.width, this.height);
+        }
     }
     move(){
-        this.y -= this.bulletSpeed;
+        if(!this.destroyed){
+            this.y -= this.bulletSpeed;
+        }
+    }
+    destroy(){
+        this.destroyed = true;
+        this.x = 100000;
+        this.width = 0;
+        this.height = 0;
     }
 }
 
 function setup(){
     setupAstroids();
     setupPlayer();
+    objectSpawner();
 }
 setup();
 
@@ -119,7 +152,7 @@ function update(){
 
     keyboardInputs();
     playerXPosTeleportEffect();
-    collisionDetection();
+    collisionDetection(bullets, astroids);
 
 }
 //every 0.01 seconds
@@ -164,20 +197,54 @@ function playerXPosTeleportEffect(){
 }
 
 function setupAstroids(){
-    for (i = 0; i < astroidAmount; i ++){
-        let x = generateRandomNum(0, canvas.width);
-        let y = generateRandomNum(0, 200);
+    
+    // for (i = 0; i < astroidAmount; i ++){
+    //     // let x = generateRandomNum(0, canvas.width);
+    //     // let y = generateRandomNum(0, 200);
+    //     // let width = generateRandomNum(50, 60);
+    //     // let height = generateRandomNum(50, 60);
+        
+    //     // //this uses hsl, only random greyscale
+    //     // let k = generateRandomNum(10, 80).toString() + "%";
+    //     // color = 'hsl(0, 0%, ' + k + ')'; 
+       
+    //     // astroids[i] = new Astroid(x, y, width, height, color);
+    // }
+}
+
+function objectSpawner(){
+    spawnObject(astroids, Astroid, spawnAmount);
+    astroidsSpawned++;
+    if(spawnRepeatRate > 1000){
+        spawnRepeatRate -= 50;
+    }
+    if(astroidsSpawned == 20 || astroidsSpawned == 50){
+        spawnAmount++;
+    }
+    console.log(spawnRepeatRate + " spawn rate");
+    console.log(astroidsSpawned + " astroids spawned");
+    setTimeout(objectSpawner, spawnRepeatRate);
+}
+
+
+function spawnObject(objectArray, objectClass, amount ){
+    
+    for(let i = 0; i < amount; i++){
+        //random Values
+        let x = generateRandomNum(0, canvas.width -300);
+        let y = -100;
         let width = generateRandomNum(50, 60);
         let height = generateRandomNum(50, 60);
-        
+    
         //this uses hsl, only random greyscale
         let k = generateRandomNum(10, 80).toString() + "%";
         color = 'hsl(0, 0%, ' + k + ')'; 
-       
-        astroids[i] = new Astroid(x, y, width, height, color);
-    }
-}
 
+        objectArray.push(new objectClass(x, y, width, height, color, Math.random()));
+    }
+    
+    
+}
 
 function createNewBullet(){
     if(coolDownEnded){
@@ -238,21 +305,25 @@ document.addEventListener('keyup', function(event){
     keys[event.keyCode] = false;
 });
 
-
-//Collision
-function collisionDetection(){
-    for (let i = 0; i < bullets.length; i++) {
-        for (let y = 0; y < astroids.length; y++) {
+//Collision Between two types of objects
+function collisionDetection(array1, array2){
+    for (let i = 0; i < array1.length; i++) {
+        for (let y = 0; y < array2.length; y++) {
             //x position collision detection
-            if(bullets[i].x + bullets[i].width >= astroids[y].x && bullets[i].x <= astroids[y].x + astroids[y].width) {
+            if(array1[i].x + array1[i].width >= array2[y].x && array1[i].x <= array2[y].x + array2[y].width) {
                 //y position collision detection
-                if(bullets[i].y + bullets[i].height >= astroids[y].y && bullets[i].y <= astroids[y].y + astroids[y].height){
-                    console.log("Collision!");
+                if(array1[i].y + array1[i].height >= array2[y].y && array1[i].y <= array2[y].y + array2[y].height){
+                    //Collision
+                    if(array1 == bullets){
+                        //Bullet actions
+                        bullets[i].destroy();
+                    }
+                    if(array2 == astroids){
+                        //Astroid actions
+                        astroids[y].destroy();
+                    }
                 }
-                
-
             }
-
 
         }
     }
